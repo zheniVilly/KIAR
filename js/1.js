a8ba65688421ac37f496a4bdab0347af5d231c76
zheniVilly <153679540+zheniVilly@users.noreply.github.com>
2024-12-20 02:04:52 +0300

    Update 1.js

--- js/1.js
+++ js/1.js
@@ -1,52 +1,4 @@
-/*document.addEventListener('DOMContentLoaded', () => {
-    const initSlider = ({
-        sliderContainerSelector,
-        sliderWrapperSelector,
-        slidesSelector,
-        gap,
-        timeout
-    }) => {
-        const sliderContainer = document.querySelector(sliderContainerSelector);
-        const sliderWrapper = document.querySelector(sliderWrapperSelector);
-        const slides = document.querySelectorAll(slidesSelector);
 
-        const sliderWidth = sliderContainer.clientWidth;
-        const slideWidth = slides[0]?.clientWidth;
-        const slidesCount = slides.length;
-        const countSlidesOnVisible = Math.floor(sliderWidth / (slideWidth + gap));
-
-        const maxSlideIndex = Math.ceil(slidesCount / countSlidesOnVisible);
-        let slideIndex = -1;
-
-        if (slideWidth && slidesCount) {
-            sliderWrapper.style.width = `${(slidesCount * (slideWidth + gap))}px`;
-        }
-
-        setInterval(() => {
-            slide();
-        }, timeout);
-
-        slide();
-
-        function slide() {
-            if (slideIndex < maxSlideIndex - 1) {
-                slideIndex++;
-            } else {
-                slideIndex = 0;
-            }
-
-            sliderWrapper.style.transform = `translateX(-${slideIndex * (slideWidth + gap)}px)`;
-        }
-    };
-
-    initSlider({
-        sliderContainerSelector: '.cards',
-        sliderWrapperSelector: '.cards',
-        slidesSelector: '.test-card',
-        gap: 50,
-        timeout: 3000
-    });
-});*/
 document.addEventListener('DOMContentLoaded', () => {
     const hamb = document.querySelector("#navbar-hamb-field");
     const popup = document.querySelector("#popup");
@@ -94,4 +46,3 @@ document.addEventListener('DOMContentLoaded', () => {
         console.error('Menu element not found');
     }
 });
-
